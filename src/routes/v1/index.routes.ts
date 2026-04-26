771b1b31c75164d5b09cd1020666c5cc950b56ae
Maitena Diaz <maitena3k6@gmail.com>
2026-04-26 16:33:40 -0300

    feat(router): update index routes:  - update auth routes path  - create users routes path  - create health routes path

--- src/routes/v1/index.routes.ts
+++ src/routes/v1/index.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import authRoutes from './auth.routes';
+import userRoutes from './user.routes';
+import healthRoutes from './health.routes';
 
 const router = Router();
-const apiRouter = Router();
+const apiRoutes = Router();
 
-apiRouter.use('/', authRoutes);
+apiRoutes.use('/auth', authRoutes);
+apiRoutes.use('/users', userRoutes);
+apiRoutes.use('/health', healthRoutes);
 
-router.use('/api/v1', apiRouter);
+router.use('/api/v1', apiRoutes);
 
 export default router;
